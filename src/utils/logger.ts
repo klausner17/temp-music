dac05ea807ceee57a773728a35f52f06c82da474
Klausner Augusto <klausner_17@yahoo.com.br>
2018-12-19 13:34:09 -0200

    logs de erro

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,18 @@
 import winston from "winston";
+import { join } from "path";
+import { existsSync, mkdirSync } from "fs";
+
+var logDir = 'logs';
+
+if (!existsSync(logDir)) {
+  mkdirSync( logDir );
+}
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' })
+      new winston.transports.File({filename: join(logDir, '/error.log'), level: 'error'})
     ]
   });
 
--- src/utils/response-error-utils.ts
+++ src/utils/response-error-utils.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
+import logger from "../utils/logger"
 
 export function sendError(error: any, errorMessage: string, statusCode: number, res: Response) {
     res.status(statusCode).json({msg: errorMessage});
-    console.log(JSON.stringify(error));
+    logger.error(`[${Date.now()}] - ${JSON.stringify(error)}`);
 }
