00bda995ce502da2fd5459f061afd31612d0a81b
klausner.pinto <klausner.pinto@ifood.com.br>
2019-05-03 15:00:22 -0300

    add simple route

--- src/routes/playslistTemperature-route.ts
+++ src/routes/playslistTemperature-route.ts
@@ -3,6 +3,7 @@ import { getTemperatureByCity, getTemperatureByCoord } from '../services/openwea
 import { getPlaylistByTemperature } from "../services/spotify";
 import { PlaylistTemperature } from '../models/PlayslistTemperature';
 import { sendError } from "../utils/response-error-utils";
+import { Buffer } from "buffer";
 
 
 var route = express.Router();
@@ -43,4 +44,75 @@ route.get('/musicByTemp/:search', (req: Request, res: Response) => {
         })
 });
 
-export default route;
+route.get("/geocode/json", (_, res: Response) => {
+    console.log(`Rand: ${Math.random()}`)
+    let response = `
+    {
+        "results" : [
+           {
+              "address_components" : [
+                 {
+                    "long_name" : "1600",
+                    "short_name" : "1600",
+                    "types" : [ "street_number" ]
+                 },
+                 {
+                    "long_name" : "Amphitheatre Pkwy",
+                    "short_name" : "Amphitheatre Pkwy",
+                    "types" : [ "route" ]
+                 },
+                 {
+                    "long_name" : "Mountain View",
+                    "short_name" : "Mountain View",
+                    "types" : [ "locality", "political" ]
+                 },
+                 {
+                    "long_name" : "Santa Clara County",
+                    "short_name" : "Santa Clara County",
+                    "types" : [ "administrative_area_level_2", "political" ]
+                 },
+                 {
+                    "long_name" : "California",
+                    "short_name" : "CA",
+                    "types" : [ "administrative_area_level_1", "political" ]
+                 },
+                 {
+                    "long_name" : "United States",
+                    "short_name" : "US",
+                    "types" : [ "country", "political" ]
+                 },
+                 {
+                    "long_name" : "94043",
+                    "short_name" : "94043",
+                    "types" : [ "postal_code" ]
+                 }
+              ],
+              "formatted_address" : "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA",
+              "geometry" : {
+                 "location" : {
+                    "lat" : 37.4224764,
+                    "lng" : -122.0842499
+                 },
+                 "location_type" : "ROOFTOP",
+                 "viewport" : {
+                    "northeast" : {
+                       "lat" : 37.4238253802915,
+                       "lng" : -122.0829009197085
+                    },
+                    "southwest" : {
+                       "lat" : 37.4211274197085,
+                       "lng" : -122.0855988802915
+                    }
+                 }
+              },
+              "place_id" : "ChIJ2eUgeAK6j4ARbn5u_wAGqWA",
+              "types" : [ "street_address" ]
+           }
+        ],
+        "status" : "OK"
+     }`
+    setTimeout(() => res.status(200).json(JSON.parse(response)), 300);
+    
+})
+
+export default route; 
